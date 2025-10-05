8466d18d4aafe567ca046021df22533a6d5881b5
Rai Rak Doi <rakdoi.phutubberk@gmail.com>
2025-10-05 10:01:38 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,12 @@
-
 // This configuration is intentionally left blank and will be populated
 // with your project's configuration when you select a Firebase project.
 // For more details, see the documentation:
 // https://firebase.google.com/docs/web/learn-more#config-object
-export const firebaseConfig = {};
-
-    
+export const firebaseConfig = {
+  projectId: 'studio-4977338751-3a7b4',
+  appId: '1:113183137665:web:f170f64c8cce7b948c5280',
+  apiKey: 'AIzaSyAE-7S0LMMXwJRJb_Y-cLM2QlpECj5RhAU',
+  authDomain: 'studio-4977338751-3a7b4.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '113183137665',
+};
